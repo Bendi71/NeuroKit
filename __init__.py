e4eca809a4b31f5bfd5ff1bf09ec71d08d9a4aed
Bendi71 <bendeguz.horvath2@stud.uni-corvinus.hu>
2025-06-09 19:38:36 +0200

    fixed auto-release workflow

--- __init__.py
+++ __init__.py
@@ -4,14 +4,19 @@ NeuroKit - A lightweight neural network library for Python.
 
 __version__ = "0.1.1"
 
-from .activations import *
-#from .convolution import Conv
-from .dense import Dense
-from .early_stopping import *
-from .losses import *
-#from .maxpool import MaxPooling
-from .metrics import *
-from .network import NeuralNetwork
-from .optimizer import *
-from .regularizer import *
-from .reshape import Reshape
+# Use a try-except block to allow importing version without loading the full package
+try:
+    from .activations import *
+    #from .convolution import Conv
+    from .dense import Dense
+    from .early_stopping import *
+    from .losses import *
+    #from .maxpool import MaxPooling
+    from .metrics import *
+    from .network import NeuralNetwork
+    from .optimizer import *
+    from .regularizer import *
+    from .reshape import Reshape
+except (ImportError, ModuleNotFoundError):
+    # This allows importing just the version without requiring all dependencies
+    pass
